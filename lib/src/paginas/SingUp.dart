39816e795a8a9a72c7c8a813354dcd73172d9166
afguevara4 <77500643+afguevara4@users.noreply.github.com>
2023-08-04 00:52:40 -0500

    configuracion manual

--- lib/src/paginas/SingUp.dart
+++ lib/src/paginas/SingUp.dart
@@ -14,12 +14,7 @@ class _SignUpPage extends State<SignUpPage> {
   final TextEditingController telefonoController = TextEditingController();
   final TextEditingController placaController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-
-  final TextEditingController validationpasswordController =
-      TextEditingController();
-
   final TextEditingController validationpasswordController = TextEditingController();
-
 
   final firebase = FirebaseFirestore.instance;
 
@@ -47,9 +42,6 @@ class _SignUpPage extends State<SignUpPage> {
   bool isTelefonoValid = true;
   bool isPlacaValid = true;
 
-  bool isPasswordValid = true;
-
-
   bool allFieldsValid = false;
 
   // Mensaje de error para el email
@@ -58,9 +50,6 @@ class _SignUpPage extends State<SignUpPage> {
   String cargoErrorMessage = '';
   String telefonoErrorMessage = '';
   String placaErrorMessage = '';
-
-  String passwordErrorMessage = '';
-
 
   @override
   Widget build(BuildContext context) {
@@ -74,49 +63,6 @@ class _SignUpPage extends State<SignUpPage> {
             _buildTextField(nameController, 'Full Name', Icons.person),
             SizedBox(height: 0),
             if (!isNameValid)
-
-              Text(
-                nameErrorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            _buildTextField(cargoController, 'Charge', Icons.work),
-            SizedBox(height: 0),
-            if (!isNameValid)
-              Text(
-                cargoErrorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            _buildTextField(correoController, 'E-mail', Icons.mail),
-            SizedBox(height: 0),
-            if (!isEmailValid)
-              Text(
-                emailErrorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            _buildTextField(telefonoController, 'Phone', Icons.phone),
-            SizedBox(height: 0),
-            if (!isTelefonoValid)
-              Text(
-                telefonoErrorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            _buildTextField(placaController, 'Placa', Icons.directions_car),
-            SizedBox(height: 0),
-            if (!isPlacaValid)
-              Text(
-                placaErrorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            _buildTextField(passwordController, 'Password', Icons.lock,
-                obscureText: true),
-            SizedBox(height: 0),
-            _buildTextField(
-                validationpasswordController, 'Password', Icons.lock,
-                obscureText: true),
-            SizedBox(height: 0),
-            if (!isPasswordValid)
-              Text(passwordErrorMessage, style: TextStyle(color: Colors.red)),
-
                 Text(
                   nameErrorMessage,
                   style: TextStyle(color: Colors.red),
@@ -153,7 +99,6 @@ class _SignUpPage extends State<SignUpPage> {
             SizedBox(height: 0),
             _buildTextField(validationpasswordController, 'Password', Icons.lock, obscureText: true),
             SizedBox(height: 0),
-
             ElevatedButton(
               onPressed: () {
                 String email = correoController.text.trim();
@@ -161,58 +106,6 @@ class _SignUpPage extends State<SignUpPage> {
                 String cargo = cargoController.text.trim();
                 String telefono = telefonoController.text.trim();
                 String placa = placaController.text.trim();
-
-                String password = passwordController.text;
-                String validationPassword = validationpasswordController.text;
-
-                if (isValidEmail(email) &&
-                    isValidName(name) &&
-                    isValidName(cargo) &&
-                    isValidPhone(telefono) &&
-                    isValidPlaca(placa)) {
-                  if (password == validationPassword) {
-                    // Las contraseñas coinciden, continuar con el registro
-                    // El email es válido, puedes realizar acciones con los datos ingresados (email y contraseña)
-                    print('E_mail: $email');
-                    print('Nombre: ${nameController.text}');
-                    print('Cargo: ${cargoController.text}');
-                    print('Correo: ${correoController.text}');
-                    print('Telefono: ${telefonoController.text}');
-                    print('Placa: ${placaController.text}');
-                    print('Contraseña: ${passwordController.text}');
-                    print(
-                        'ValContraseña: ${validationpasswordController.text}');
-                    // Reiniciar el mensaje de error en caso de que esté mostrándose actualmente
-                    setState(() {
-                      isEmailValid = true;
-                      isNameValid = true;
-                      isCargoValid = true;
-                      isTelefonoValid = true;
-                      isPlacaValid = true;
-                      emailErrorMessage = '';
-                      nameErrorMessage = '';
-                      cargoErrorMessage = '';
-                      telefonoErrorMessage = '';
-                      placaErrorMessage = '';
-                    });
-
-                    registroUsuario();
-
-                    // Navegar de vuelta a la página de inicio de sesión
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  } else {
-                    // Las contraseñas no coinciden, mostrar un mensaje de error
-                    setState(() {
-                      isPasswordValid = false;
-                      passwordErrorMessage = 'Las contraseñas no coinciden';
-                    });
-                  }
-                } else {
-                  // Mostrar mensajes de error según la validación fallida
-
                 
                 if (isValidEmail(email) && isValidName(name) && isValidName(cargo) && isValidPhone(telefono) && isValidPlaca(placa) )  {
                   // El email es válido, puedes realizar acciones con los datos ingresados (email y contraseña)
@@ -249,7 +142,6 @@ class _SignUpPage extends State<SignUpPage> {
                   
                 } else {
                 // Mostrar mensajes de error según la validación fallida
-
                   if (!isValidEmail(email)) {
                     setState(() {
                       isEmailValid = false;
@@ -261,12 +153,7 @@ class _SignUpPage extends State<SignUpPage> {
                     // Puedes adaptar este mensaje de acuerdo a tus necesidades
                     setState(() {
                       isNameValid = false;
-
-                      nameErrorMessage =
-                          'Nombre incorrecto. Solo se permiten letras.';
-
                       nameErrorMessage = 'Nombre incorrecto. Solo se permiten letras.';
-
                     });
                   }
                   if (!isValidName(cargo)) {
@@ -274,12 +161,7 @@ class _SignUpPage extends State<SignUpPage> {
                     // Puedes adaptar este mensaje de acuerdo a tus necesidades
                     setState(() {
                       isCargoValid = false;
-
-                      cargoErrorMessage =
-                          'Cargo incorrecto. Solo se permiten letras.';
-
                       cargoErrorMessage = 'Cargo incorrecto. Solo se permiten letras.';
-
                     });
                   }
                   if (!isValidName(telefono)) {
@@ -316,13 +198,7 @@ class _SignUpPage extends State<SignUpPage> {
     );
   }
 
-
-  Widget _buildTextField(
-      TextEditingController controller, String label, IconData icon,
-      {bool obscureText = false}) {
-
   Widget _buildTextField(TextEditingController controller, String label, IconData icon, {bool obscureText = false}) {
-
     return TextFormField(
       controller: controller,
       obscureText: obscureText,
@@ -332,13 +208,7 @@ class _SignUpPage extends State<SignUpPage> {
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
         ),
-
-        contentPadding: EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: 20), // Ajusta el tamaño de los campos de texto
-
         contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 20), // Ajusta el tamaño de los campos de texto
-
       ),
     );
   }
@@ -349,7 +219,7 @@ class _SignUpPage extends State<SignUpPage> {
     return emailRegex.hasMatch(email);
   }
 
-  // Función de validación de nombre (solo letras)
+  // Función de validación de nombre (solo letras) ---- Revisar los espacios
   bool isValidName(String name) {
     final RegExp nameRegex = RegExp(r'^[a-zA-Z]+$');
     return nameRegex.hasMatch(name);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,23 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-
-import cloud_firestore
-import firebase_auth
-import firebase_core
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-
 import cloud_firestore
 import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-
 }
